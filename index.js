988b93da1d27ae217d62a4dfd72132a35d103500
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-20 22:07:31 +0200

    get users

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const http = require('http');
 const bodyParser = express.json();
 const yup = require('yup');
 const PORT = 3000;
-
 const server = http.createServer(app);
-
 const USER_SCHEMA = yup.object({
     firstName: yup.string().required(),
    lastName: yup.string().required(),
@@ -14,9 +12,7 @@ const USER_SCHEMA = yup.object({
    password: yup.string().required(),
    isSubscribed: yup.boolean()
 })
-
 const db = [];
-
 app.post('/user', bodyParser, async (req, res, next) => {
    const {body} = req;
    try {
@@ -32,13 +28,16 @@ app.post('/user', bodyParser, async (req, res, next) => {
     delete user.password;
     res.status(201).send(user);
 }); 
+app.get('/users/', (req, res, next) => {
+    res.send(db);
+})
 
 
 server.listen(PORT, ()=> {
     console.log(`App is started on port ${PORT}`)
 });
 
-
+/* написати ручку на отримання всіх "зареєстрованих" юзерів */
 
 /*
 Реєстрація юзера
@@ -46,5 +45,5 @@ server.listen(PORT, ()=> {
 +2. Распарсити (перетворити у об'єкт) дані з запиту
 +3. Перевірити (провалідувати) отримані дані
 +4. Зберегти юзера 
-5. Надіслати у відповідь готовий об'єкт зареєстрованого юзера
++5. Надіслати у відповідь готовий об'єкт зареєстрованого юзера
 */
