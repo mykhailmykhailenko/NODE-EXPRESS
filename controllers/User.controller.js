ed20084d24adab705a267582d5c1f0e966d780ce
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-20 22:14:04 +0200

    added getAll and getOne, Hometask here!

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -1,17 +1,20 @@
 const User = require('../models/User');
-
-
 module.exports.createUser = async (req, res, next) => {
     const {body} = req;
     const createdUser = await new User(body);
     const result = await createdUser.save();
      res.status(201).send(result);
 }
-
 module.exports.getAllUsers = async (req, res, next) => {
    const users = await User.findAll();
 }
 
-module.exports.getOneUser = () => {}
+module.exports.getOneUser = async (req, res, next) => {
+        const {params: {userId}} = req;
+        const foundedUser = await User.findOne(Number(userId));
+        res.send(foundedUser);
+}
+
 module.exports.updateUser = () => {}
+
 module.exports.deleteUser = () => {}
--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const http = require('http');
 const bodyParser = express.json();
 const {validateUser} = require('./mw/validation.mw');
 const UserController = require('./controllers/User.controller');
-
 const PORT = 3000;
-
 const server = http.createServer(app);
 
 app.post('/user', bodyParser, validateUser, UserController.createUser); 
 app.get('/users/', UserController.getAllUsers);
-
+app.get('/users/:userId', UserController.getOneUser);
+app.put('/users/:userId', UserController.updateUser);
 
 server.listen(PORT, ()=> {
     console.log(`App is started on port ${PORT}`)
 });
+
+
+/*
+Додати ендпоінт (ручку) для Update та Delete юзерів.
+*/
