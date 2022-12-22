53ef2b9d8e62ea2ca4fe76826f7baeec4f8a1ce8
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-12-22 13:37:14 +0200

    error handling

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -1,20 +1,57 @@
 const User = require('../models/User');
+
+
 module.exports.createUser = async (req, res, next) => {
-    const {body} = req;
-    const createdUser = await new User(body);
-    const result = await createdUser.save();
-     res.status(201).send(result);
+        try {
+                const {body} = req;
+                const createdUser = await new User(body);
+                const result = await createdUser.save();
+                 res.status(201).send(result);
+        } catch(error) {
+                next(error);
+        }
+
 }
+
 module.exports.getAllUsers = async (req, res, next) => {
-   const users = await User.findAll();
+        try {
+                const users = await User.findAll();
+                res.status(200).send(users);
+        }catch(error) {
+                next(error)
+        }
 }
 
 module.exports.getOneUser = async (req, res, next) => {
-        const {params: {userId}} = req;
-        const foundedUser = await User.findOne(Number(userId));
-        res.send(foundedUser);
+        try {
+                const {params: {userId}} = req;
+                const foundedUser = await User.findOne(Number(userId));
+                res.send(foundedUser);
+        } catch (error) {
+                next(error)
+        }
+
 }
 
-module.exports.updateUser = () => {}
+module.exports.updateUser = async (req, res, next) => {
+        try {
+                const {params: {userId}, body} = req;
+                const foundedUser = await User.findOne(Number(userId));
+                const updated = await foundedUser.update(body);
+                res.status(200).send(updated);
+        } catch(error) {
+                next(error);
+        }
+}
 
-module.exports.deleteUser = () => {}
+module.exports.deleteUser = async (req, res, next) => {
+        try {
+                const {params: {userId}} = req;
+                const foundedUser = await User.findOne(Number(userId));
+               const result = await foundedUser.delete();
+                res.status(200).send('User deleted');
+        } catch(error) {
+              next(error);
+        }
+
+}
--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const db = new Map();
-
 class User {
     constructor({firstName, lastName, email, password, isSubscribe = false}){
         this.firstName = firstName;
@@ -9,10 +8,8 @@ class User {
         this.isSubscribe = isSubscribe;
         this.id = db.size + 1;
         this.createdAt = new Date();
-
         return Promise.resolve(this);
     }
-
     async save () {
         db.set(this.id, this);
         return this;
@@ -20,24 +17,18 @@ class User {
 
     async update(updateValues) {
         const oldUser = db.get(this.id);
-        const newUser = new User({...oldUser, ...updateValues});
+        const newUser = await new User({...oldUser, ...updateValues});
         db.set(this.id, newUser);
         return newUser;
     }
     async delete() {
         return db.delete(this.id);
     }
-
     static async findOne(id) {
         return db.get(id);
     }
-
     static async findAll() {
         return [...db.values()]
     }
-
 }
-
-
-
 module.exports = User;
